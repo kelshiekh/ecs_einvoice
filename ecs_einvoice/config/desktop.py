f2618e81713099f96249b87591df46a057724372
Khaled El-Shiekh <kelshiekh@gmail.com>
2024-08-06 02:43:24 +0300

    Add files via upload

---
+++ ecs_einvoice/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Ecs Einvoice",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Ecs Einvoice")
+		}
+	]
